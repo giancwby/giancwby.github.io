423248572162a56222f42a0da91d51374b4e0205
giancwby <gcaedo.gc@gmail.com>
2022-05-30 16:24:58 +0800

    Created portfolio repository

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+document.querySelectorAll('section').forEach(
+    (section) => {
+        gsap.fromTo(section, {x:-20, opacity: 0}, {scrollTrigger: {
+            trigger: section,
+            start: 'top center'
+        }, duration:2, x:0, opacity: 1, ease: 'power3'});
+    }
+)
+
+gsap.fromTo('nav', {y:-20, opacity: 0}, {duration:2, y:0, opacity: 1, ease: 'power3'});
+
+gsap.fromTo('footer', {y:-20, opacity: 0}, {scrollTrigger: 'footer',duration:2, y:0, opacity: 1, ease: 'power3'});
