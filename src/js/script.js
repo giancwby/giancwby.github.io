c4f3e8f78f88a21b7e7e95039217d34e4c564009
giancwby <gcaedo.gc@gmail.com>
2022-06-03 10:46:04 +0800

    Updated animation to stagger

--- src/js/script.js
+++ src/js/script.js
@@ -6,15 +6,27 @@ window.addEventListener('load', () => {
 
 gsap.registerPlugin(ScrollTrigger);
 
-document.querySelectorAll('section').forEach(
-    (section) => {
-        gsap.fromTo(section, {x:-20, opacity: 0}, {scrollTrigger: {
-            trigger: section,
-            start: 'top 73%'
-        }, duration:1, x:0, opacity: 1, ease: 'power3'});
-    }
-)
+const sections = document.querySelectorAll('section');
+
+for (const section of sections) {
+
+    const sectionChildren = section.children;
+
+    const tl = gsap.timeline().fromTo(sectionChildren, {x:-20, opacity:0}, {duration: 1, x:0, opacity:1,stagger:0.2})
+
+    ScrollTrigger.create({
+        trigger: section,
+        start: 'top 73%',
+        animation: tl
+    })
+}
 
 gsap.fromTo('nav', {y:-20, opacity: 0}, {duration:2, y:0, opacity: 1, ease: 'power3'});
 
 gsap.fromTo('footer', {y:-20, opacity: 0}, {scrollTrigger: 'footer',duration:2, y:0, opacity: 1, ease: 'power3'});
+
+
+
+
+
+
