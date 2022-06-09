b16d26d6dbf36fbcbd4042d51ab7685af5f9e64a
giancwby <gcaedo.gc@gmail.com>
2022-06-09 17:24:59 +0800

    Added branding category page

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,7 @@ for (const section of sections) {
     })
 }
 
+
 gsap.fromTo('nav', {y:-20, opacity: 0}, {duration:2, y:0, opacity: 1, ease: 'power3'});
 
 gsap.fromTo('footer', {y:-20, opacity: 0}, {scrollTrigger: 'footer',duration:2, y:0, opacity: 1, ease: 'power3'});
